6d0b37269fabf3902a4fb9cd6e3db2154edc1025
shankerabhigyan <shankerabhigyan@gmail.com>
2024-02-05 18:14:39 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,4 @@
+const uuid = require('uuid'); // import the uuid package 
+const {v4 : uuidv4} = require('uuid'); // import the uuid package and rename the v4 function to uuidv4
+
+console.log(uuidv4()); // print the uuid to the console 
