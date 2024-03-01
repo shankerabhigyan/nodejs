22fb9b1f871cc51043c5b7fb3f1422e63352ebb9
shankerabhigyan <shankerabhigyan@gmail.com>
2024-03-01 17:56:17 +0530

    updated

--- express/app.js
+++ express/app.js
@@ -1,10 +1,18 @@
 const express = require('express')
-const app = express()
+const path = require('path')
 
-app.get('/',(req,res)=>{
-    
+const app = express()
+// setup static and middleware
+app.use(express.static('./public')) // this will serve all the files in the public folder
+
+// app.get('/',(req,res)=>{
+//     res.status(200).sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+// })
+
+app.all('*',(req,res)=>{    
+    res.status(404).send('Resource not found')
 })
 
 app.listen(5000,()=>{
-    console.log('server is listening on port 5000')
+    console.log('server listening on port 5000')
 });
